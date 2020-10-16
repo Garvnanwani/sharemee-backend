7e95db9d72828dbf33557558ea075fbcaef73d5d
Garv nanwani <garvnanwani88@gmail.com>
2020-10-17 01:08:19 +0530

    Update files.js

--- routes/files.js
+++ routes/files.js
@@ -5,14 +5,15 @@ const File = require('../models/file');
 const { v4: uuidv4 } = require('uuid');
 
 let storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'uploads/'),
-    filename: (req, file, cb) => {
-        const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1E9)}${path.extname(file.originalname)}`;
-        cb(null, uniqueName)
-    },
-});
-
-let upload = multer({ storage, limits: { fileSize: 1000000 * 100 }, }).single(''); //100mb
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+let upload = multer({ storage: storage })
 
 router.post('/', (req, res) => {
     upload(req, res, async (err) => {
